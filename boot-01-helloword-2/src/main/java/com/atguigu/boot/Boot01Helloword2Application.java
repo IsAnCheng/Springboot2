12ace96c6a9114d606f8417fa3be01cc388719c3
Ancheng <Ancheng@qq.com>
2022-11-23 16:42:32 +0800

    first commit

---
+++ boot-01-helloword-2/src/main/java/com/atguigu/boot/Boot01Helloword2Application.java
@@ -0,0 +1,15 @@
+package com.atguigu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot01Helloword2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Boot01Helloword2Application.class, args);
+
+    }
+
+}
