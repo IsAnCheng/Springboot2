44facfb6701991472f90a98fc848de5bfec42a1a
Ancheng <Ancheng@qq.com>
2022-11-23 16:43:10 +0800

    second commit

--- boot-01-helloword-2/src/main/java/com/atguigu/boot/controller/Git.java
+++ boot-01-helloword-2/src/main/java/com/atguigu/boot/controller/Git.java
@@ -3,5 +3,6 @@ package com.atguigu.boot.controller;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello!!Git!");
+        System.out.println("hello!!Git1!");
     }
 }
